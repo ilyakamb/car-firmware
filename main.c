6159d646cc0bc7fdbd73bd04dff62efda29e0f42
Ilya Kambarov <admin@onescript.ru>
2014-03-19 11:53:03 +0300

    Comments added

--- main.c
+++ main.c
@@ -8,27 +8,26 @@
 int main(void)
 {
 
-    D5_Out;
+    D5_Out;		// Set motor pins mode to 'output'
     D6_Out;
     D10_Out;
     D11_Out;
 
     D13_Out;
 
-    int buffer;
-    char result;
-
-    USART0Init();
-
-start: ;
-
-    result = USART0ReceiveByte();
-
-
-        if(result=='r')
-        {
-            buffer = USART0ReceiveByte();
-            buffer = (int) buffer -48;
+    int buffer;						// We receive a char from UART, the command name:
+    char result;					// 'r' -> rigth two motors, forward
+							// 'l' -> left two motors, forward
+    USART0Init(); // Initialize	UART 	//	|----->	// 'q' -> right two motors, backward			
+					//	|	// 'm' -> left two motors, backward
+start: ;				//	|	// 'f' -> lights
+					//	|
+    result = USART0ReceiveByte(); //------------|							
+							
+        if(result=='r')					
+        {						
+            buffer = USART0ReceiveByte();			// Receive parameter, speed [0:9] for motors
+            buffer = (int) buffer -48;			
             setPWM(6,0);
             if(buffer == 0){ setPWM(5,0); }
             else if(buffer == 9){ setPWM(5, 255); }
@@ -37,9 +36,9 @@ start: ;
 
         else if(result=='l')
         {
-            buffer = USART0ReceiveByte();
+            buffer = USART0ReceiveByte();			// Receive parameter, speed [0:9] for motors
             buffer = (int) buffer -48;
-            setPWM(11,0);
+            setPWM(11,0);					
             if(buffer == 0){ setPWM(10,0); }
             else if(buffer == 9){ setPWM(10, 255);  }
             else { setPWM(10,buffer*28); }
@@ -47,7 +46,7 @@ start: ;
 
         else if(result=='q')
         {
-            buffer = USART0ReceiveByte();
+            buffer = USART0ReceiveByte();			// Receive parameter, speed [0:9] for motors
             buffer = (int) buffer -48;
             setPWM(5,0);
             if(buffer == 0){ setPWM(6, 0); }
@@ -55,7 +54,7 @@ start: ;
             else { setPWM(6, buffer*28); }
         }
 
-        else if(result=='m')
+        else if(result=='m')					// Receive parameter, speed [0:9] for motors
         {
             buffer = USART0ReceiveByte();
             buffer = (int) buffer -48;
@@ -67,9 +66,9 @@ start: ;
 
         else if(result=='f')
 	{
-	    buffer = USART0ReceiveByte();
-            buffer = (int) buffer -48;
-            if(buffer == 0){ D13_Low; }
+	    buffer = USART0ReceiveByte();			// Receive parameter for lights
+            buffer = (int) buffer -48;				// '0' -> Off
+            if(buffer == 0){ D13_Low; }				// '1' -> On
             else { D13_High; }
 	}
 
