76b49be54c409fdbc375fb7a1b1b9682a9835968
ilyakamb <admin@onescript.ru>
2014-03-26 13:49:37 +0400

    Some improvements to PWM

--- PWMlib.h
+++ PWMlib.h
@@ -1,31 +1,52 @@
 void setPWM(int pin, int level)
 {
-    if(pin == 6)       // OCR0A	register
+
+    if(pin == 6)       // OCR0A
     {
         OCR0A = level;
-        TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
+        if(level == 0) TCCR0A |= (1 << COM0A1);
+        else TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
         TCCR0B |= (1 << CS00);
     }
 
-    else if(pin == 5)       // OCR0B	register
+    else if(pin == 5)       // OCR0B
     {
         OCR0B = level;
-        TCCR0A |= (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
+        if(level == 0) TCCR0A |= (1 << COM0B1);
+        else TCCR0A |= (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
         TCCR0B |= (1 << CS00);
     }
 
-    else if(pin == 11)      // OCR2A	register
+    else if(pin == 9)      // OCR1A
     {
-        OCR2A = level;
-        TCCR2A |= (1 << COM2A1) | (1 << WGM01) | (1 << WGM00);
-        TCCR2B |= (1 << CS01);
+        OCR1A = level;
+        if(level == 0) TCCR1A &= ~(1 << COM1A1);
+        else TCCR1A |= (1 << COM1A1) | (1 << WGM01) | (1 << WGM00);
+        TCCR1B |= (1 << CS10);
     }
 
-    else if(pin == 10)      // OCR1B	register
+    else if(pin == 10)      // OCR1B
     {
         OCR1B = level;
-        TCCR1A |= (1 << COM1B1) | (1 << WGM21) | (1 << WGM20);
-        TCCR1B |= (1 << CS20);
+        if(level == 0) TCCR1A &= ~(1 << COM1B1);
+        else TCCR1A |= (1 << COM1B1) | (1 << WGM01) | (1 << WGM00);
+        TCCR1B |= (1 << CS10);
+    }
+
+    else if(pin == 11)      // OCR2A
+    {
+        OCR2A = level;
+        if(level == 0) TCCR2A &= ~(1 << COM2A1);
+        else TCCR2A |= (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);
+        TCCR2B |= (1 << CS20);
+    }
+
+    else if(pin == 3)      // OCR2B
+    {
+        OCR2B = level;
+        if(level == 0) TCCR2A &= ~(1 << COM2B1);
+        else TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+        TCCR2B |= (1 << CS20);
     }
 
     else {}
--- main.c
+++ main.c
@@ -8,67 +8,64 @@
 int main(void)
 {
 
-    D5_Out;		// Set motor pins mode to 'output'
+    D5_Out;
     D6_Out;
     D10_Out;
     D11_Out;
 
     D13_Out;
 
-    int buffer;						// We receive a char from UART, the command name:
-    char result;					// 'r' -> rigth two motors, forward
-							// 'l' -> left two motors, forward
-    USART0Init(); // Initialize	UART 	//	|----->	// 'q' -> right two motors, backward			
-					//	|	// 'm' -> left two motors, backward
-start: ;				//	|	// 'f' -> lights
-					//	|
-    result = USART0ReceiveByte(); //------------|							
-							
-        if(result=='r')					
-        {						
-            buffer = USART0ReceiveByte();			// Receive parameter, speed [0:9] for motors
-            buffer = (int) buffer -48;			
+    int buffer;
+    char result;
+
+    USART0Init();
+
+start: ;
+
+    result = USART0ReceiveByte();
+
+
+        if(result=='r')
+        {
+            buffer = USART0ReceiveByte();
+            buffer = (int) buffer -48;
             setPWM(6,0);
-            if(buffer == 0){ setPWM(5,0); }
-            else if(buffer == 9){ setPWM(5, 255); }
+            if(buffer == 9){ setPWM(5, 255); }
             else { setPWM(5, buffer*28); }
         }
 
         else if(result=='l')
         {
-            buffer = USART0ReceiveByte();			// Receive parameter, speed [0:9] for motors
+            buffer = USART0ReceiveByte();
             buffer = (int) buffer -48;
-            setPWM(11,0);					
-            if(buffer == 0){ setPWM(10,0); }
-            else if(buffer == 9){ setPWM(10, 255);  }
+            setPWM(11,0);
+            if(buffer == 9){ setPWM(10, 255);  }
             else { setPWM(10,buffer*28); }
         }
 
         else if(result=='q')
         {
-            buffer = USART0ReceiveByte();			// Receive parameter, speed [0:9] for motors
+            buffer = USART0ReceiveByte();
             buffer = (int) buffer -48;
             setPWM(5,0);
-            if(buffer == 0){ setPWM(6, 0); }
-            else if(buffer == 9){ setPWM(6, 255); }
+            if(buffer == 9){ setPWM(6, 255); }
             else { setPWM(6, buffer*28); }
         }
 
-        else if(result=='m')					// Receive parameter, speed [0:9] for motors
+        else if(result=='m')
         {
             buffer = USART0ReceiveByte();
             buffer = (int) buffer -48;
             setPWM(10,0);
-            if(buffer == 0){ setPWM(11, 0); }
-            else if(buffer == 9){ setPWM(11, 255); }
+            if(buffer == 9){ setPWM(11, 255); }
             else { setPWM(11, buffer*28); }
         }
 
         else if(result=='f')
 	{
-	    buffer = USART0ReceiveByte();			// Receive parameter for lights
-            buffer = (int) buffer -48;				// '0' -> Off
-            if(buffer == 0){ D13_Low; }				// '1' -> On
+            buffer = USART0ReceiveByte();
+            buffer = (int) buffer -48;
+            if(buffer == 0){ D13_Low; }
             else { D13_High; }
 	}
 
